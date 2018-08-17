744aaf5487bd18a4b405b7a0b5dd081c24579afb
Sönke Hahn <soenkehahn@gmail.com>
2018-08-17 23:10:52 +0200

    remove unneeded mutability

--- src/lib.rs
+++ src/lib.rs
@@ -38,7 +38,7 @@ pub struct Receiver<T> {
 impl<T> Receiver<T> {
     /// Returns the last sent value. Returns `None` if
     /// no value was sent since the last call to `recv`.
-    pub fn recv(&mut self) -> Option<T> {
+    pub fn recv(&self) -> Option<T> {
         let mut result = None;
         for t in self.receiver.try_iter() {
             result = Some(t);
@@ -75,14 +75,14 @@ mod tests {
 
         #[test]
         fn allows_to_send_one_value() {
-            let (sender, mut receiver) = skipchannel();
+            let (sender, receiver) = skipchannel();
             sender.send("foo");
             assert_eq!(receiver.recv(), Some("foo"));
         }
 
         #[test]
         fn allows_to_send_values_between_threads() {
-            let (sender, mut receiver) = skipchannel();
+            let (sender, receiver) = skipchannel();
             sender.send("foo");
             let read_result = parallel(move || receiver.recv());
             assert_eq!(read_result, Some("foo"));
@@ -90,13 +90,13 @@ mod tests {
 
         #[test]
         fn yields_none_when_nothing_is_sent() {
-            let (_sender, mut receiver): (Sender<i32>, Receiver<i32>) = skipchannel();
+            let (_sender, receiver): (Sender<i32>, Receiver<i32>) = skipchannel();
             assert_eq!(receiver.recv(), None);
         }
 
         #[test]
         fn skips_all_values_but_the_last() {
-            let (sender, mut receiver) = skipchannel();
+            let (sender, receiver) = skipchannel();
             sender.send("foo");
             sender.send("bar");
             let read_result = parallel(move || receiver.recv());
@@ -105,7 +105,7 @@ mod tests {
 
         #[test]
         fn returns_none_when_a_sent_value_is_already_consumed() {
-            let (sender, mut receiver) = skipchannel();
+            let (sender, receiver) = skipchannel();
             sender.send("foo");
             receiver.recv();
             assert_eq!(receiver.recv(), None);
